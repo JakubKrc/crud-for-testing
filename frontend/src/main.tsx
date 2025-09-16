70ae857fe869c70dc25944d7b2b36ea9427a9110
JakubKrc <96569372+JakubKrc@users.noreply.github.com>
2025-09-16 13:14:19 +0200

    feat(routing): add NotFoundPage for handling nonexistent routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import Test1 from './pages/test1.tsx'
+import NotFoundPage from './pages/notFound.tsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 const router = createBrowserRouter([
   {path:"/",element: <App />},
-  {path:"/test1",element: <Test1 />}
+  {path:"/test1",element: <Test1 />},
+  {path:"*", element: <NotFoundPage />}
 ])
 
 createRoot(document.getElementById('root')!).render(
