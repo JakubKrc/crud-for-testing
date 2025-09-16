29d6780f2d655813764f3744c5927f08293ef260
JakubKrc <96569372+JakubKrc@users.noreply.github.com>
2025-09-16 13:00:55 +0200

    feat(routing): add /test1 route

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import Test1 from './pages/test1.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {path:"/",element: <App />},
+  {path:"/test1",element: <Test1 />}
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ frontend/src/pages/test1.tsx
@@ -0,0 +1,9 @@
+function test1() {
+    return (
+        <>
+         test1
+        </>
+    )
+}
+
+export default test1
